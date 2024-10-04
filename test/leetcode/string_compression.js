39451b1dce7447fb25330013c8236f582cca2973
Eric Butera <eric@ericbutera.com>
2024-10-04 14:51:07 -0400

    feat(leetcode): compress ii

---
+++ test/leetcode/string_compression.js
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/string-compression/description/
+
+var compress = function (chars) {
+  const out = []
+  let count = 0;
+  for (let x = 0; x < chars.length; x++) {
+    let current = chars[x];
+    let hasNext = x + 1 < chars.length
+    let next = (hasNext) ? chars[x + 1] : ''
+
+    if (current != next) { // change detected
+      out.push(current)
+      appendTotal(out, count)
+      count = 0;
+    } else {
+      count++;
+    }
+  }
+
+  return out
+};
+
+function appendTotal(out, count) {
+  if (count > 0) {
+    // Note that group lengths that are 10 or longer will be split into multiple characters in chars
+    let total = count + 1
+    if (total >= 10) {
+      for (num of total.toString()) {
+        out.push(num.toString())
+      }
+    } else {
+      out.push(total.toString())
+    }
+  }
+}
+
+const assert = require('assert')
+describe('compress', () => {
+  it('handles example 1', () => {
+    assert.deepEqual(compress(["a", "a", "b", "b", "c", "c", "c"]), ["a", "2", "b", "2", "c", "3"])
+  })
+
+  it('handles example 2', () => {
+    assert.deepEqual(compress(['a']), ['a'])
+  })
+
+  it('handles example 3', () => {
+    assert.deepEqual(compress(["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]), ["a", "b", "1", "2"])
+  })
+})
